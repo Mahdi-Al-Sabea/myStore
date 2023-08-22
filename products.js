8ee2cef3eac08368cb9df2174dc92231febe88e8
Mahdi-Al-Sabea <mahdisabeh12345@gmail.com>
2023-08-22 15:08:12 +0300

    replacing local files images with remote ones on github

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 const products = [
     {
       id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      image: "C:/Users/mahdi/OneDrive/Desktop/MyCodes/web development/own projects/myStore/myStore/products/athletic-cotton-socks-6-pairs.jpg",
+      image: "https://raw.githubusercontent.com/Mahdi-Al-Sabea/myStore/main/products/athletic-cotton-socks-6-pairs.jpg",
       name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
       rating: {
         stars: 4.5,
@@ -16,7 +16,7 @@ const products = [
     },
     {
       id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      image: "C:/Users/mahdi/OneDrive/Desktop/MyCodes/web development/own projects/myStore/myStore/products/intermediate-composite-basketball.jpg",
+      image: "Chttps://raw.githubusercontent.com/Mahdi-Al-Sabea/myStore/main/productsintermediate-composite-basketball.jpg",
       name: "Intermediate Size Basketball",
       rating: {
         stars: 4,
@@ -30,7 +30,7 @@ const products = [
     },
     {
       id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-      image: "C:/Users/mahdi/OneDrive/Desktop/MyCodes/web development/own projects/myStore/myStore/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+      image: "https://raw.githubusercontent.com/Mahdi-Al-Sabea/myStore/main/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
       name: "Adults Plain Cotton T-Shirt - 2 Pack",
       rating: {
         stars: 4.5,
@@ -47,7 +47,7 @@ const products = [
     },
     {
       id: "54e0eccd-8f36-462b-b68a-8182611d9add",
-      image: "C:/Users/mahdi/OneDrive/Desktop/MyCodes/web development/own projects/myStore/myStore/products/black-2-slot-toaster.jpg",
+      image: "https://raw.githubusercontent.com/Mahdi-Al-Sabea/myStore/main/products/black-2-slot-toaster.jpg",
       name: "2 Slot Toaster - Black",
       rating: {
         stars: 5,
@@ -62,7 +62,7 @@ const products = [
     },
     {
       id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
-      image: "C:/Users/mahdi/OneDrive/Desktop/MyCodes/web development/own projects/myStore/myStore/products/6-piece-white-dinner-plate-set.jpg",
+      image: "https://raw.githubusercontent.com/Mahdi-Al-Sabea/myStore/main/products/6-piece-white-dinner-plate-set.jpg",
       name: "6 Piece White Dinner Plate Set",
       rating: {
         stars: 4,
@@ -77,7 +77,7 @@ const products = [
     },
     {
       id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
-      image: "C:/Users/mahdi/OneDrive/Desktop/MyCodes/web development/own projects/myStore/myStore/products/6-piece-non-stick-baking-set.webp",
+      image: "https://raw.githubusercontent.com/Mahdi-Al-Sabea/myStore/main/products/6-piece-non-stick-baking-set.webp",
       name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
       rating: {
         stars: 4.5,
@@ -91,7 +91,7 @@ const products = [
     },
     {
       id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
-      image: "C:/Users/mahdi/OneDrive/Desktop/MyCodes/web development/own projects/myStore/myStore/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
+      image: "https://raw.githubusercontent.com/Mahdi-Al-Sabea/myStore/main/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
       name: "Plain Hooded Fleece Sweatshirt",
       rating: {
         stars: 4.5,
@@ -105,7 +105,7 @@ const products = [
       ]
     },
     {    id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
-      image: "C:/Users/mahdi/OneDrive/Desktop/MyCodes/web development/own projects/myStore/myStore/products/straw-sunhat.webp",
+      image: "https://raw.githubusercontent.com/Mahdi-Al-Sabea/myStore/main/products/straw-sunhat.webp",
       name: "Luxury Towel Set - Graphite Gray",
       rating: {
         stars: 4.5,
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 my store project
+"C:/Users/mahdi/OneDrive/Desktop/MyCodes/web development/own projects/myStore/myStore/products/athletic-cotton-socks-6-pairs.jpg"
+products
